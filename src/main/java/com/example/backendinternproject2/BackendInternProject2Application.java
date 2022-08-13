5b58119623f7679c0e921ec31f5a63f1eb35ca98
Naserpour Alireza <anaserpoor98@gmail.com>
2022-08-13 18:56:34 +0430

    Init

---
+++ src/main/java/com/example/backendinternproject2/BackendInternProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendinternproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInternProject2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendInternProject2Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/backendinternproject2/BackendInternProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendinternproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInternProject2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
